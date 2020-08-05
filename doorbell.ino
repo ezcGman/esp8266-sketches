74b047f584ab2f4019f9e1c4d992f050fa5b7daf
Andy Hofmann <ich@andy-hofmann.com>
2020-08-05 08:36:12 +0200

    Fixed typos in comments

--- doorbell.ino
+++ doorbell.ino
@@ -79,7 +79,7 @@ void setup() {
 
   Serial.begin(9600);
 
-  // The Wi-FI library has auto-reocnnect. So we simply call it in setup() and there is no need to always check the conenction in loop()
+  // The Wi-Fi library has auto-reconnect. So we simply call it in setup() and there is no need to always check the conenction in loop()
   connectWifi();
 
   mqttClient.setServer(mqttServer, mqttPort);
