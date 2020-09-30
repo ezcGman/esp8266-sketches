2cb96177e7f498b8498e5ef295f32f8f60b06662
Andy Hofmann <ich@andy-hofmann.com>
2020-09-30 08:16:11 +0200

    Update soil_moisture_sensor.ino
    New soil moisture sensor version which utilizes a capacitive soil moisture sensor and uses a digital GPIO to switch a transistor to turn on/off the sensor

--- soil-moisture-sensor/soil_moisture_sensor.ino
+++ soil-moisture-sensor/soil_moisture_sensor.ino
@@ -1,5 +1,8 @@
 #include <ESP8266WiFi.h>
+// Using https://pubsubclient.knolleary.net/
 #include <PubSubClient.h>
+// Using https://github.com/mcxiaoke/ESPDateTime
+#include <DateTime.h>
 
 
 const char* ssid     = "not-gonna-tell-ya";
@@ -10,8 +13,15 @@ const int mqttPort = 1883;
 const char* mqttUser = "mqtt";
 const char* mqttPassword = "stop-looking-for-my-passwords";
 
-uint8_t analogMoisturePin = A0;
-uint8_t digitalMoisturePin = D2;
+const byte analogMoisturePin = A0;
+const byte enableSensorPin = D2;
+
+// Measured myself, using this guide: https://media.digikey.com/pdf/Data%20Sheets/DFRobot%20PDFs/SEN0193_Web.pdf
+const int airValue = 910;
+const int waterValue = 412;
+// The original script devides into "Very wet", "Wet" and "Dry". But we will only care about "Dry"
+// int intervals = (airValue - waterValue) / 3;
+const int plantDryThreshold = ((airValue - waterValue) * 0.6) + waterValue;
 
 const byte maxConnTries = 15;
 
@@ -31,12 +41,12 @@ void connectWifi() {
       Serial.printf("Connecting to Wi-Fi %s ...\n", ssid);
       delay(1000);
     }
-    
+
     if (WiFi.status() != WL_CONNECTED && wifiConnTries >= maxConnTries) {
       Serial.println("Maximum number of Wi-Fi connection attempts reached. Rebooting...");
       ESP.restart();
     }
-  
+
     Serial.printf("Connected to Wi-Fi! IP address: %s\n", WiFi.localIP().toString().c_str());
   }
 }
@@ -64,17 +74,11 @@ void connectMqtt() {
   }
 }
 
-void setup() { 
+void setup() {
+  pinMode(enableSensorPin, OUTPUT);
+  digitalWrite(enableSensorPin, HIGH);
+
   Serial.begin(9600);
-
-  pinMode(digitalMoisturePin, INPUT);
-
-  float moistureValue = analogRead(analogMoisturePin);
-  // 0 == wet, 1 == dry
-  int isWet = !digitalRead(digitalMoisturePin);
-
-  // Not really working as expected. Putting the sensor in water says ~60% O_o
-  // float moisturePercentage = ( 100.00 - ( (sensorValue/1023.00) * 100.00 ) );
 
   // The Wi-FI library has auto-reocnnect. So we simply call it in setup() and there is no need to always check the conenction in loop()
   connectWifi();
@@ -82,16 +86,34 @@ void setup() {
   mqttClient.setServer(mqttServer, mqttPort);
   connectMqtt();
 
+  float soilMoistureValue = analogRead(analogMoisturePin);
+  Serial.println(soilMoistureValue);
+
   char charMoistureValue[4];
-  int intMoistureValue = moistureValue;
-  char charIsWet[1];
+  int intMoistureValue = soilMoistureValue;
   sprintf(charMoistureValue, "%d", intMoistureValue);
-  sprintf(charIsWet, "%d", isWet);
   mqttClient.publish("home-assistant/plant-sensors/herbals/moisture", charMoistureValue);
+
+  int isWet = 1;
+  if(soilMoistureValue > plantDryThreshold) {
+    isWet = 0;
+  }
+  Serial.printf("Is wet: %d\n", isWet);
+
+  char charIsWet[1];
+  sprintf(charIsWet, "%d", isWet);
   mqttClient.publish("home-assistant/plant-sensors/herbals/wet", charIsWet);
-  
-  Serial.println(moistureValue);
-  Serial.println(isWet);
+
+  DateTime.begin();
+  if (!DateTime.isTimeValid()) {
+    Serial.println("Failed to get time from server.");
+  }
+  String strDateTime = DateTime.format("%Y-%m-%dT%H:%M:%S%z");
+  char charDateTime[strDateTime.length()];
+  strDateTime.toCharArray(charDateTime, strDateTime.length());
+  mqttClient.publish("home-assistant/plant-sensors/herbals/lastUpdated", charDateTime);
+
+  digitalWrite(enableSensorPin, LOW);
 
   delay(100);
   ESP.deepSleep(10*60e6); // Deep Sleep for 10 minutes
